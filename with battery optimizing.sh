e96332bdc20e5475d1819ad8aa551c1b5a93101e
l889-coder <99336165+l889-coder@users.noreply.github.com>
2022-12-08 20:34:45 +0100

    Update with battery optimizing.sh

--- with battery optimizing.sh
+++ with battery optimizing.sh
@@ -1,2 +1,16 @@
+sudo apt update
+sudo apt upgrade
+sudo apt-get update
+
+sudo apt install
+sudo update-grub
+sudo apt install preload
+sudo add-apt-repository ppa:apt-fast/stable
+sudo apt-get update
+sudo apt-get install apt-fast
+sudo gedit /etc/apt/apt.conf.d/00aptitude
+sudo apt update
+sudo apt autoremove
+sudo apt-get clean
 sudo apt install tlp tlp-rdw
 sudo tlp start
