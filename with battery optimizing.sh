1ec6ef16ab1fc5c14424e2971ea246865ea44ece
l889-coder <99336165+l889-coder@users.noreply.github.com>
2022-12-12 17:30:28 +0100

    Update with battery optimizing.sh

--- with battery optimizing.sh
+++ with battery optimizing.sh
@@ -5,9 +5,9 @@ sudo apt-get update
 sudo apt install
 sudo update-grub
 sudo apt install preload
-sudo add-apt-repository ppa:apt-fast/stable
+
 sudo apt-get update
-sudo apt-get install apt-fast
+
 sudo apt --purge autoremove
 sudo apt update
 sudo apt autoremove
